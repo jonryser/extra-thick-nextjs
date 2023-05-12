4e25a92f14485cc6f95cfb883fd1db7deccb0c65
Jon Ryser <241263+jonryser@users.noreply.github.com>
2023-05-12 14:40:26 -0700

    Add environment variables to reset script.

--- reset_env_vars.sh
+++ reset_env_vars.sh
@@ -35,10 +35,34 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Unset any previously set environment variables.
 unset API_ROOT
+unset CLOUDINARY_ENABLED
+unset CLOUDINARY_API_KEY
+unset CLOUDINARY_API_SECRET
+unset CLOUDINARY_CLOUD_FOLDER_NAME
+unset CLOUDINARY_CLOUD_NAME
+unset DB_HOST
+unset DB_NAME
+unset DB_PASS
+unset DB_PORT
+unset DB_USER
+unset DB_URL
 unset DOT_ENV_FILE
+unset EMAIL_SENDING_IS_ENABLED
+unset HUSKY
+unset NEXT_PUBLIC_API_URL
+unset NEXT_PUBLIC_CLOUDINARY_ENABLED
+unset NEXT_PUBLIC_ENV
+unset NEXTAUTH_SECRET
+unset NO_AUTO_START
+unset NODE_VERSION
+unset NODE_ENV
 unset SERVER_DOMAIN
 unset SERVER_PORT
-unset SERVER_PROTOCAL
+unset SERVER_PROTOCOL
+unset BASE_URL
+unset NEXTAUTH_URL
+unset SENDGRID_API_KEY
+unset SENDGRID_EMAIL_SENDER
 
 >&2 echo -e "${GREEN}* Environment variables unset.${NC}"
 
