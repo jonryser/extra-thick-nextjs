d7ce5cde5d35d9d2cd7ea329305960640f84069c
Jon Ryser <241263+jonryser@users.noreply.github.com>
2023-11-04 13:51:52 -0700

    Added image to Calendar event

--- src/utils/Calendar.ts
+++ src/utils/Calendar.ts
@@ -1,6 +1,12 @@
 import { VCalendar, parseIcsCalendar } from 'ts-ics'
 import { ICS_URL } from 'utils/constants'
 
+export function getGoogleDriveImage(url: string) {
+	const parsed = new URL(url)
+	const id = parsed.searchParams.get('id')
+	return `https://drive.google.com/uc?export=view&id=${id}`
+}
+
 export async function getIcs(id: string): Promise<VCalendar | undefined> {
 	try {
 		const res: VCalendar = await fetch(ICS_URL(id), {
